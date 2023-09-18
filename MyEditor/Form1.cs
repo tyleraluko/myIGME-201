64660a032b4a5fa3b93c419822cb26aa3067fb79
Tyler Aluko <tyleraluko@gmail.com>
2023-09-17 21:43:32 -0400

    Tutorial #1

---
+++ MyEditor/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Tyler Aluko
+ * IGME.201 - Tutorial #1
+ * I haven't a clue what's goin on here, but I'll act like I do!
+ */
+namespace MyEditor {
+
+    public partial class Form1 : Form {
+        
+        public Form1() {
+            
+            InitializeComponent();
+
+            //event handlers on mouse click for menu items
+            
+            //file
+            this.newToolStripMenuItem.Click += new EventHandler(NewToolStripMenuItem__Click); //new option
+            this.openToolStripMenuItem.Click += new EventHandler(OpenToolStripMenuItem__Click); //open option
+            this.saveToolStripMenuItem.Click += new EventHandler(SaveToolStripMenuItem__Click); //save option
+            this.exitToolStripMenuItem.Click += new EventHandler(ExitToolStripMenuItem__Click); //exit option
+
+            //edit
+            this.copyToolStripMenuItem.Click += new EventHandler(CopyToolStripMenuItem__Click); //copy option
+            this.cutToolStripMenuItem.Click += new EventHandler(CutToolStripMenuItem__Click); //cut option
+            this.pasteToolStripMenuItem.Click += new EventHandler(PasteToolStripMenuItem__Click); //paste option
+
+            //tool strip
+            this.toolStrip.ItemClicked += new ToolStripItemClickedEventHandler(ToolStrip__ItemClicked);
+            
+            this.Text = "MyEditor";
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e) {
+            
+            //clears rich text box contents
+            richTextBox.Clear();
+            this.Text = "MyEditor"; //then reset text
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e) {
+            
+            //i have no idea what i'm doing tbh
+            if(openFileDialog.ShowDialog() == DialogResult.OK) {
+            
+                RichTextBoxStreamType richTextBoxStreamType = RichTextBoxStreamType.RichText;
+                
+                if(openFileDialog.FileName.ToLower().Contains(".txt")) {
+                    //if .txt, convert to plain text?
+                    RichTextBoxStreamType richTextBoxStreamType = RichTextBoxStreamType.PlainText;
+                }
+                
+                richTextBox.LoadFile(openFileDialog.FileName, RichTextBoxStreamType);
+
+                //set to name of file being edited
+                this.Text = "MyEditor (" + openFileDialog.FileName + ")";
+            }
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e) {
+
+            saveFileDialog.FileName = openFileDialog.FileName;
+
+            //i have no idea what i'm doing tbh
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                RichTextBoxStreamType richTextBoxStreamType = RichTextBoxStreamType.RichText;
+
+                if (saveleDialog.FileName.ToLower().Contains(".txt"))
+                {
+                    //if .txt, convert to plain text?
+                    RichTextBoxStreamType richTextBoxStreamType = RichTextBoxStreamType.PlainText;
+                }
+
+                richTextBox.SaveFile(saveFileDialog.FileName, richTextBoxStreamType);
+
+                //set to name of file being edited
+                this.Text = "MyEditor (" + saveFileDialog.FileName + ")";
+            }
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e) {
+            richTextBox.Copy();
+        }
+
+        private void CutToolStripMenuItem_Click(object sender, EventArgs e) {
+            richTextBox.Cut();
+        }
+
+        private void PasteToolStripMenuItem_Click(object sender, EventArgs e) {
+            richTextBox.Paste();
+        }
+
+        private void ToolStrip__ItemClicked(object sender, ToolStripItemClickedEventArgs e) {
+            FontStyle fontStyle = FontStyle.Regular;
+
+            ToolStripButton toolStripButton = null;
+
+            if (e.ClickedItem == this.boldToolStripButton) {
+                fontStyle = FontStyle.Bold;
+                toolStripButton = this.boldToolStripButton;
+            }
+            else if (e.ClickedItem == this.italicsToolStripButton) {
+                fontStyle = FontStyle.Italic;
+                toolStripButton = this.italicsToolStripButton;
+            }
+            else if (e.ClickedItem == this.underlineToolStripButton) {
+                fontStyle = FontStyle.Underline;
+                toolStripButton = this.underlineToolStripButton;
+            }
+            else if (e.ClickedItem == this.colorToolStripButton) {
+                if (colorDialog.ShowDialog() == DialogResult.OK) {
+                    richTextBox.SelectionColor = colorDialog.Color;
+                    colorToolStripButton.BackColor = colorDialog.Color;
+                }
+            }
+
+            if (fontStyle != FontStyle.Regular) {
+                toolStripButton.Checked = !toolStripButton.Checked;
+                SetSelectionFont(fontStyle, toolStripButton.Checked);
+            }
+
+        }
+
+        private void SetSelectionFont(FontStyle fontStyle, bool bSet) {
+            
+            Font newFont = null;
+            Font selectionFont = null;
+
+            selectionFont = richTextBox.SelectionFont;
+
+            if (selectionFont == null) {
+                selectionFont = richTextBox.Font;
+            }
+
+            if (bSet) {
+                newFont = new Font(selectionFont, selectionFont.Style | fontStyle);
+            }
+            else {
+                newFont = new Font(selectionFont, selectionFont.Style & ~fontStyle);
+            }
+
+            this.richTextBox.SelectionFont = newFont;
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ PE7-Sanabria/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MadLibs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setting up starting variables
+            int numLibs = 0;
+            int cntr = 0;
+            int nChoice = 0;
+            string finalStory = "";
+            string resultString = "";
+            //Setting up the StreamReader for the external text file
+            StreamReader input;
+
+
+            input = new StreamReader("c:\\templates\\MadLibsTemplate.txt");
+
+            string textLine = null;
+            while ((textLine = input.ReadLine()) != null)
+            {
+                ++numLibs;
+            }
+            input.Close();
+
+            string[] madLibs = new string[numLibs];
+            input = new StreamReader("c:\\templates\\MadLibsTemplate.txt");
+
+            textLine = null;
+            while ((textLine = input.ReadLine()) != null)
+            {
+                // set this array element to the current line of the template file
+                madLibs[cntr] = textLine;
+
+                // replace the "\\n" tag with the newline escape character
+                madLibs[cntr] = madLibs[cntr].Replace("\\n", "\n");
+
+                ++cntr;
+            }
+            input.Close();
+            //Prompting user for which they want to play. 
+            bool acceptableValue = false;
+
+            while (acceptableValue == false)
+            {
+                Console.WriteLine("Which Mad Libs would you like to play? 1-" + numLibs);
+
+                if (nChoice >= numLibs)
+                {
+                    Console.WriteLine("This isn't a permitted value.");
+                    continue;
+                }
+                else if (nChoice <= numLibs)
+                {
+                    nChoice = int.Parse(input.ReadLine());
+                    acceptableValue = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a proper number.");
+                    continue;
+                }
+                }
+            }
+
+
+            string[] words = madLibs[nChoice].Split(' ');
+
+            foreach (string word in words)
+            {
+
+                if (word[0] == '{')
+                {
+                    string replacementWord = word.Replace("{", "").Replace("}", "").Replace("_", "");
+                    //Prompts user for a replacement word
+                    Console.Write("Input a {0}: ", replacementWord);
+                    //Adds the response into the string
+                    finalStory += Console.ReadLine();
+                }
+                // else append word to the result string
+                else
+                {
+                    finalStory += word;
+                }
+                resultString = finalStory;
+
+            }
+        }
+    }
+}
