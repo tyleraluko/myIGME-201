7db0d87b101672d47b97d7db8a6ea9d14a7120e6
Tyler Aluko <tyleraluko@gmail.com>
2023-10-29 22:53:26 -0400

    EX2

---
+++ EX2-Aluko_Friend/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Tyler Aluko
+ * IGME.201 - Unit Test #2
+ * Converts struct Friend to public class Friend w/ the same functionality.
+ */
+namespace EX2_Aluko_Friend
+{
+    public class Friend
+    {
+        public string name;
+        public string greeting;
+        public DateTime birthdate;
+        public string address;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Friend friend = new Friend();
+            Friend enemy = new Friend();
+
+            // create my friend Charlie Sheen
+            friend.name = "Charlie Sheen";
+            friend.greeting = "Dear Charlie";
+            friend.birthdate = DateTime.Parse("1967-12-25");
+            friend.address = "123 Any Street, NY NY 12202";
+
+            // now he has become my enemy
+            enemy = new Friend();
+            enemy.name = friend.name;
+            enemy.greeting = friend.greeting;
+            enemy.birthdate = friend.birthdate;
+            enemy.address = friend.address;
+
+            // set the enemy greeting and address without changing the friend variable
+            enemy.greeting = "Sorry Charlie";
+            enemy.address = "Return to sender.  Address unknown.";
+
+            Console.WriteLine($"friend.greeting => enemy.greeting: {friend.greeting} => {enemy.greeting}");
+            Console.WriteLine($"friend.address => enemy.address: {friend.address} => {enemy.address}");
+        }
+    }
+}
